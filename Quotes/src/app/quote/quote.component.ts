5f24cc16fbd9d1c8c9e1e44d4e4dd9107cdf48b2
carolyne <pcmaunda@gmail.com>
2022-02-11 22:14:28 +0300

    changes on github

--- Quotes/src/app/quote/quote.component.ts
+++ Quotes/src/app/quote/quote.component.ts
@@ -10,9 +10,9 @@ import {Quote} from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [
-    new Quote(1,'African Quotes','Only a fool tests the depth of a river with both feet',new Date(2020,3,14)), 
-    new Quote (2,'Common Quotes','A stich in timesaves nine',new Date(2020,3,14)),
-    new Quote(3, 'easy Quotes','Get busy living or get busy dying',new Date(2020,3,14)),
+    new Quote(1,'African Quotes','Only a fool tests the depth of a river with both feet',new Date(2020,4,10)), 
+    new Quote (2,'Common Quotes','A stich in timesaves nine',new Date(2020,5,11)),
+    new Quote(3, 'easy Quotes','Get busy living or get busy dying',new Date(2020,6,16)),
     new Quote (4,'scholarly Quotes','Many of life’s failures are people who did not realize how close they were to success when they gave up.',new Date(2020,3,14)),
     
   ];
